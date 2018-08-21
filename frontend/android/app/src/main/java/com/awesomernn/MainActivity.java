4747d298a14e93eee85475e22949af0e9bb53681
siuhangw <wshiahb@gmail.com>
2018-08-21 12:46:04 +0800

    Frontend project upload
    Frontend proejct inital commit

---
+++ frontend/android/app/src/main/java/com/awesomernn/MainActivity.java
@@ -0,0 +1,9 @@
+package com.awesomernn;
+
+import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
+
+// public class MainActivity extends ReactActivity {
+public class MainActivity extends SplashActivity {
+
+}
