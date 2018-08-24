3aeb96db57b4739a346955391b24cd80aef1f89e
siuhangw <wshiahb@gmail.com>
2018-08-24 17:15:41 +0800

    Valuation page changes

--- frontend/PropertyValuation/containers/Valuation.tsx
+++ frontend/PropertyValuation/containers/Valuation.tsx
@@ -19,6 +19,9 @@ import {
 } from "react-native";
 
 import { NavigationComponentProps } from "react-native-navigation";
+import { Navigation } from "react-native-navigation";
+import Auth from "../containers/Auth";
+Navigation.registerComponent("example.auth", () => Auth);
 
 interface Props extends NavigationComponentProps {}
 
@@ -32,13 +35,25 @@ export default class Valuation extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Image
-          style={{ width: 50, height: 50 }}
-          source={{
-            uri:
-              "https://facebook.github.io/react-native/docs/assets/favicon.png"
-          }}
-        />
+        <TouchableOpacity
+          onPress={() =>
+            this.props.navigator.showModal({
+              screen: "example.auth", // unique ID registered with Navigation.registerScreen
+              title: "Auth", // title of the screen as appears in the nav bar (optional)
+              passProps: {}, // simple serializable object that will pass as props to the modal (optional)
+              navigatorStyle: {}, // override the navigator style for the screen, see "Styling the navigator" below (optional)
+              animationType: "slide-up" // 'none' / 'slide-up' , appear animation for the modal (optional, default 'slide-up')
+            })
+          }
+        >
+          <Image
+            style={{ width: 50, height: 50 }}
+            source={{
+              uri:
+                "https://facebook.github.io/react-native/docs/assets/favicon.png"
+            }}
+          />
+        </TouchableOpacity>
         <Text style={styles.welcome}>Valuation</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
