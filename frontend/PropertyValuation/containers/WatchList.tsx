5befa570ca817c2da0b382adb338212e4d38fdb7
siuhangw <wshiahb@gmail.com>
2018-08-24 17:23:02 +0800

    WatchList changed & redux update

--- frontend/PropertyValuation/containers/WatchList.tsx
+++ frontend/PropertyValuation/containers/WatchList.tsx
@@ -10,78 +10,122 @@
 import * as React from "react";
 import { Component } from "react";
 import {
-  Platform,
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
-  FlatList,
   Dimensions,
-  Slider
+  Image
 } from "react-native";
-
 import { NavigationComponentProps } from "react-native-navigation";
+// import { connect } from "react-redux";
+// import { Dispatch } from "redux";
+// import {
+//   ILoginActions,
+//   logInUser,
+//   logOutUser
+// } from "../components/auth/action";
+// import { IRootState } from "../redux/store";
+// import { IAuthUser } from "../models/models";
 
-interface Props extends NavigationComponentProps {}
+interface IWatchListProps {
+  // loginIn: (auth: IAuthUser) => void;
+  // auth: IAuthUser;
+}
 
-// type Props = {};
-export default class WatchList extends Component<Props> {
-  render() {
+interface IWatchListStates {
+  loginMsg: string;
+}
+
+export default class WatchList extends React.Component<
+  IWatchListProps,
+  IWatchListStates
+> {
+  constructor(props: IWatchListProps) {
+    super(props);
+
+    this.state = {
+      loginMsg: "Please sign up / login to use this feature!!!!!!!!!!"
+    };
+  }
+
+  public render() {
     return (
       <View style={styles.container}>
-        <FlatList
-          data={[
-            { key: "Devin" },
-            { key: "Jackson" },
-            { key: "James" },
-            { key: "Joel" },
-            { key: "John" },
-            { key: "Jillian" },
-            { key: "Jimmy" }
-          ]}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              key={item.key}
-              onPress={() =>
-                this.props.navigator.switchToTab({
-                  tabIndex: 1 // Here is the tab index
-                })
-              }
-            >
-              <Text key={item.key} style={styles.item}>
-                {item.key}
-              </Text>
-            </TouchableOpacity>
-          )}
-        />
-        <Slider
-          style={{
-            width: Dimensions.get("window").width * 0.8,
-            padding: 40
-          }}
-          maximumValue={100}
-          minimumValue={0}
-        />
-        <Slider
-          style={{
-            width: Dimensions.get("window").width * 0.8,
-            padding: 40
-          }}
-          maximumValue={100}
-          minimumValue={0}
-        />
-        <Slider
-          style={{
-            width: Dimensions.get("window").width * 0.8,
-            padding: 40
-          }}
-          maximumValue={100}
-          minimumValue={0}
-        />
+        {/* <TouchableOpacity
+          onPress={() =>
+            this.props.navigator.showModal({
+              screen: "example.auth", // unique ID registered with Navigation.registerScreen
+              title: "Auth", // title of the screen as appears in the nav bar (optional)
+              passProps: {}, // simple serializable object that will pass as props to the modal (optional)
+              navigatorStyle: {}, // override the navigator style for the screen, see "Styling the navigator" below (optional)
+              animationType: "slide-up" // 'none' / 'slide-up' , appear animation for the modal (optional, default 'slide-up')
+            })
+          }
+        > */}
+        <TouchableOpacity onPress={this.loginYouIn}>
+          <Image
+            style={{ width: 50, height: 50 }}
+            source={require("../src/icons/IC-Add-Circle-24px.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.loginYouOut}>
+          <Image
+            style={{ width: 50, height: 50 }}
+            source={require("../src/icons/IC-Remove-Circle-Outline-24px.png")}
+          />
+        </TouchableOpacity>
+        <Text>{this.state.loginMsg}</Text>
       </View>
     );
   }
+
+  private loginYouIn = () => {
+    // Do something
+    this.setState({
+      loginMsg: "Yes, you are now login"
+    });
+  };
+
+  private loginYouOut = () => {
+    // Do something
+    this.setState({
+      loginMsg: "Please sign up / login to use this feature!!!!!!!!!!"
+    });
+  };
+
+  // private loginYouIn = () => {
+  //   this.props.loginIn(this.props.auth);
+  // };
 }
+
+// const mapStateToProps = (state: IRootState) => {
+//   return {
+//     // links: state.links.linkList
+//     auth: {
+//       login: {
+//         username: "string",
+//         email: "string",
+//         password: "string",
+//         isLoggedIn: true,
+//         isLoggingIn: true
+//       }
+//     }
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch: Dispatch<ILoginActions>) => {
+//   return {
+//     loginIn: (auth: IAuthUser) => {
+//       dispatch(logInUser(auth));
+//     }
+//   };
+// };
+
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(WatchList);
 
 const styles = StyleSheet.create({
   container: {
