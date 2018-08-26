e38baee27700479f3391cc2198721d96fe2484a6
siuhangw <wshiahb@gmail.com>
2018-08-26 13:18:57 +0800

    Data model added

---
+++ frontend/PropertyValuation/models/models.ts
@@ -0,0 +1,62 @@
+export interface IUser {
+  gender: string;
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  location: {
+    street: string;
+    city: string;
+    state: string;
+    postcode: Number;
+    coordinates: {
+      latitude: string;
+      longitude: string;
+    };
+    timezone: {
+      offset: string;
+      description: string;
+    };
+  };
+  email: string;
+  login: {
+    uuid: string;
+    username: string;
+    password: string;
+    salt: string;
+    md5: string;
+    sha1: string;
+    sha256: string;
+  };
+  dob: {
+    date: string;
+    age: 23;
+  };
+  registered: {
+    date: string;
+    age: 8;
+  };
+  phone: string;
+  cell: string;
+  id: {
+    name: string;
+    value: string;
+  };
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+  nat: string;
+}
+
+export interface IAuthUser {
+  login: {
+    username: string;
+    email: string;
+    password: string;
+    isLoggedIn: boolean;
+    isLoggingIn: boolean;
+  };
+}
