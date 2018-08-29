72b7abfb5131cc79df74d72a5eb3a56b58d1009d
Alam Husnan <kd.dunk81@gmail.com>
2018-08-29 09:35:56 +0800

    completed service and router

--- backend/data/insert_data_app.js
+++ backend/data/insert_data_app.js
@@ -15,7 +15,7 @@ const knex = require('knex')(knexFile)
 
 
 //data stream hong kong
-let instream = fs.createReadStream('./data/28housefinal1.csv');
+let instream = fs.createReadStream('/home/alam/Property-Valuation/backend/recent_data.csv');
 let outstream = new stream;
 outstream.readable = true;
 outstream.writeable = true;
@@ -62,150 +62,150 @@ rl.on('line', function (line) {
     })
 });
 
-//data stream kowloon
+// //data stream kowloon
 
-let instream1 = fs.createReadStream('./data/28housefinal2.csv');
-let outstream1 = new stream;
-outstream1.readable = true;
-outstream1.writeable = true;
+// let instream1 = fs.createReadStream('./data/28housefinal2.csv');
+// let outstream1 = new stream;
+// outstream1.readable = true;
+// outstream1.writeable = true;
 
-var rl1 = readline.createInterface({
-    input: instream1,
-    output: outstream1,
-    terminal: false
-});
+// var rl1 = readline.createInterface({
+//     input: instream1,
+//     output: outstream1,
+//     terminal: false
+// });
 
-const asyncQueue1 = asyncModule.queue(async function(real_estate_data1, cb1) {
-    await 
-    console.log(real_estate_data1);
-    let query1 = await knex
-    .select()
-    .from('real_estate')
-    .where('real_estate.addr', real_estate_data1.addr)
-    .andWhere('catfathername', real_estate_data1.catfathername)
-    .andWhere('catname', real_estate_data1.catname)
-    if(query1.length >= 1 ) {
-        console.log('information already there' + query1[0])
-    } else {
-        // console.log('writing to knex')
-        return await knex
-            .insert({
-                addr: real_estate_data1.addr,
-                catfathername: real_estate_data1.catfathername,
-                catname: real_estate_data1.catname,
-                area: real_estate_data1.area
-            }).into('real_estate')
-    }
+// const asyncQueue1 = asyncModule.queue(async function(real_estate_data1, cb1) {
+//     await 
+//     console.log(real_estate_data1);
+//     let query1 = await knex
+//     .select()
+//     .from('real_estate')
+//     .where('real_estate.addr', real_estate_data1.addr)
+//     .andWhere('catfathername', real_estate_data1.catfathername)
+//     .andWhere('catname', real_estate_data1.catname)
+//     if(query1.length >= 1 ) {
+//         console.log('information already there' + query1[0])
+//     } else {
+//         // console.log('writing to knex')
+//         return await knex
+//             .insert({
+//                 addr: real_estate_data1.addr,
+//                 catfathername: real_estate_data1.catfathername,
+//                 catname: real_estate_data1.catname,
+//                 area: real_estate_data1.area
+//             }).into('real_estate')
+//     }
 
-    return cb1;
+//     return cb1;
 
-},1);
+// },1);
 
-rl1.on('line', function (line1) {
-    let real_estate_data1 = line1.split(",")
-    asyncQueue1.push({
-            addr: real_estate_data1[1],
-            catfathername: real_estate_data1[2],
-            catname: real_estate_data1[3],
-            area: real_estate_data1[9],
-    })
-});
-
-
-//data stream new territories
-
-let instream2 = fs.createReadStream('./data/28housefinal3.csv');
-let outstream2 = new stream;
-outstream2.readable = true;
-outstream2.writeable = true;
-
-var rl2 = readline.createInterface({
-    input: instream2,
-    output: outstream2,
-    terminal: false
-});
-
-const asyncQueue2 = asyncModule.queue(async function(real_estate_data2, cb2) {
-    await 
-    console.log(real_estate_data2);
-    let query2 = await knex
-    .select()
-    .from('real_estate')
-    .where('real_estate.addr', real_estate_data2.addr)
-    .andWhere('catfathername', real_estate_data2.catfathername)
-    .andWhere('catname', real_estate_data2.catname)
-    if(query2.length >= 1 ) {
-        console.log('information already there' + query2[0])
-    } else {
-        // console.log('writing to knex')
-        return await knex
-            .insert({
-                addr: real_estate_data2.addr,
-                catfathername: real_estate_data2.catfathername,
-                catname: real_estate_data2.catname,
-                area: real_estate_data2.area
-            }).into('real_estate')
-    }
-
-    return cb2;
-
-},1);
-
-rl2.on('line', function (line2) {
-    let real_estate_data2 = line2.split(",")
-    asyncQueue2.push({
-            addr: real_estate_data2[1],
-            catfathername: real_estate_data2[2],
-            catname: real_estate_data2[3],
-            area: real_estate_data2[9],
-    })
-});
+// rl1.on('line', function (line1) {
+//     let real_estate_data1 = line1.split(",")
+//     asyncQueue1.push({
+//             addr: real_estate_data1[1],
+//             catfathername: real_estate_data1[2],
+//             catname: real_estate_data1[3],
+//             area: real_estate_data1[9],
+//     })
+// });
 
 
-//data stream lantau
-let instream3 = fs.createReadStream('./data/28housefinal170.csv');
-let outstream3 = new stream;
-outstream3.readable = true;
-outstream3.writeable = true;
+// //data stream new territories
 
-var rl3 = readline.createInterface({
-    input: instream3,
-    output: outstream3,
-    terminal: false
-});
+// let instream2 = fs.createReadStream('./data/28housefinal3.csv');
+// let outstream2 = new stream;
+// outstream2.readable = true;
+// outstream2.writeable = true;
 
-const asyncQueue3 = asyncModule.queue(async function(real_estate_data3, cb3) {
-    await 
-    console.log(real_estate_data3);
-    let query3 = await knex
-    .select()
-    .from('real_estate')
-    .where('real_estate.addr', real_estate_data3.addr)
-    .andWhere('catfathername', real_estate_data3.catfathername)
-    .andWhere('catname', real_estate_data3.catname)
-    if(query.length3 >= 1 ) {
-        console.log('information already there' + query3[0])
-    } else {
-        // console.log('writing to knex')
-        return await knex
-            .insert({
-                addr: real_estate_data3.addr,
-                catfathername: real_estate_data3.catfathername,
-                catname: real_estate_data3.catname,
-                area: real_estate_data3.area
-            }).into('real_estate')
-    }
+// var rl2 = readline.createInterface({
+//     input: instream2,
+//     output: outstream2,
+//     terminal: false
+// });
 
-    return cb3;
+// const asyncQueue2 = asyncModule.queue(async function(real_estate_data2, cb2) {
+//     await 
+//     console.log(real_estate_data2);
+//     let query2 = await knex
+//     .select()
+//     .from('real_estate')
+//     .where('real_estate.addr', real_estate_data2.addr)
+//     .andWhere('catfathername', real_estate_data2.catfathername)
+//     .andWhere('catname', real_estate_data2.catname)
+//     if(query2.length >= 1 ) {
+//         console.log('information already there' + query2[0])
+//     } else {
+//         // console.log('writing to knex')
+//         return await knex
+//             .insert({
+//                 addr: real_estate_data2.addr,
+//                 catfathername: real_estate_data2.catfathername,
+//                 catname: real_estate_data2.catname,
+//                 area: real_estate_data2.area
+//             }).into('real_estate')
+//     }
 
-},1);
+//     return cb2;
 
-rl3.on('line', function (line3) {
-    let real_estate_data3 = line3.split(",")
-    asyncQueue3.push({
-            addr: real_estate_data3[1],
-            catfathername: real_estate_data3[2],
-            catname: real_estate_data3[3],
-            area: real_estate_data3[9],
-    })
-});
+// },1);
+
+// rl2.on('line', function (line2) {
+//     let real_estate_data2 = line2.split(",")
+//     asyncQueue2.push({
+//             addr: real_estate_data2[1],
+//             catfathername: real_estate_data2[2],
+//             catname: real_estate_data2[3],
+//             area: real_estate_data2[9],
+//     })
+// });
+
+
+// //data stream lantau
+// let instream3 = fs.createReadStream('./data/28housefinal170.csv');
+// let outstream3 = new stream;
+// outstream3.readable = true;
+// outstream3.writeable = true;
+
+// var rl3 = readline.createInterface({
+//     input: instream3,
+//     output: outstream3,
+//     terminal: false
+// });
+
+// const asyncQueue3 = asyncModule.queue(async function(real_estate_data3, cb3) {
+//     await 
+//     console.log(real_estate_data3);
+//     let query3 = await knex
+//     .select()
+//     .from('real_estate')
+//     .where('real_estate.addr', real_estate_data3.addr)
+//     .andWhere('catfathername', real_estate_data3.catfathername)
+//     .andWhere('catname', real_estate_data3.catname)
+//     if(query.length3 >= 1 ) {
+//         console.log('information already there' + query3[0])
+//     } else {
+//         // console.log('writing to knex')
+//         return await knex
+//             .insert({
+//                 addr: real_estate_data3.addr,
+//                 catfathername: real_estate_data3.catfathername,
+//                 catname: real_estate_data3.catname,
+//                 area: real_estate_data3.area
+//             }).into('real_estate')
+//     }
+
+//     return cb3;
+
+// },1);
+
+// rl3.on('line', function (line3) {
+//     let real_estate_data3 = line3.split(",")
+//     asyncQueue3.push({
+//             addr: real_estate_data3[1],
+//             catfathername: real_estate_data3[2],
+//             catname: real_estate_data3[3],
+//             area: real_estate_data3[9],
+//     })
+// });
--- backend/data/insert_data_app2.js
+++ backend/data/insert_data_app2.js
@@ -14,7 +14,7 @@ const knex = require('knex')(knexFile)
 
 //data stream hong kong
 
-let instream = fs.createReadStream('./data/28housefinal1.csv');
+let instream = fs.createReadStream('./recent_data.csv');
 let outstream = new stream;
 outstream.readable = true;
 outstream.writeable = true;
@@ -81,201 +81,201 @@ rl.on('line', function (line) {
 
 //data stream kowloon
 
-let instream1 = fs.createReadStream('./data/28housefinal2.csv');
-let outstream1 = new stream;
-outstream1.readable = true;
-outstream1.writeable = true;
+// let instream1 = fs.createReadStream('./data/28housefinal2.csv');
+// let outstream1 = new stream;
+// outstream1.readable = true;
+// outstream1.writeable = true;
 
-var rl1 = readline.createInterface({
-    input: instream1,
-    output: outstream1,
-    terminal: false
-});
+// var rl1 = readline.createInterface({
+//     input: instream1,
+//     output: outstream1,
+//     terminal: false
+// });
 
-const asyncQueue1 = asyncModule.queue(async function(his_trans_data1, cb1) {
-    await
-    console.log(his_trans_data1);
-    let query1 = await knex
-    .select('re_id')
-    .from('real_estate')
-    .where('real_estate.addr', his_trans_data1.addr)
-    .andWhere('catfathername', his_trans_data1.catfathername)
-    .andWhere('catname', his_trans_data1.catname)
+// const asyncQueue1 = asyncModule.queue(async function(his_trans_data1, cb1) {
+//     await
+//     console.log(his_trans_data1);
+//     let query1 = await knex
+//     .select('re_id')
+//     .from('real_estate')
+//     .where('real_estate.addr', his_trans_data1.addr)
+//     .andWhere('catfathername', his_trans_data1.catfathername)
+//     .andWhere('catname', his_trans_data1.catname)
     
-        console.log('do i work?');
-        console.log(query1[0].re_id)
+//         console.log('do i work?');
+//         console.log(query1[0].re_id)
 
-        return await knex
-            .insert({
-                re_id: query1[0].re_id,
+//         return await knex
+//             .insert({
+//                 re_id: query1[0].re_id,
 
-                id: his_trans_data1.id,
+//                 id: his_trans_data1.id,
 
-                block: his_trans_data1.block,
+//                 block: his_trans_data1.block,
 
-                rootid: his_trans_data1.rootid,
+//                 rootid: his_trans_data1.rootid,
 
-                price_value: his_trans_data1.price_value,
+//                 price_value: his_trans_data1.price_value,
 
-                date: his_trans_data1.date,
+//                 date: his_trans_data1.date,
 
-                sq_price: his_trans_data1.sq_price,
+//                 sq_price: his_trans_data1.sq_price,
 
-                winloss: his_trans_data1.winloss,
+//                 winloss: his_trans_data1.winloss,
 
-                img_url: his_trans_data1.img_url
+//                 img_url: his_trans_data1.img_url
 
-            }).into('historical_transaction')
+//             }).into('historical_transaction')
                     
-}, 15);
+// }, 15);
 
-rl1.on('line', function (line1) {
-    let his_trans_data1 = line1.split(",")
-    asyncQueue1.push({
-        id: his_trans_data1[0],
-        addr: his_trans_data1[1],
-        catfathername: his_trans_data1[2],
-        catname: his_trans_data1[3],
-        block: his_trans_data1[4],
-        rootid: his_trans_data1[5],
-        price_value: his_trans_data1[6],
-        date: his_trans_data1[7],
-        sq_price: his_trans_data1[8],
-        winloss: his_trans_data1[10],
-        img_url: his_trans_data1[11],
-    })
-});
+// rl1.on('line', function (line1) {
+//     let his_trans_data1 = line1.split(",")
+//     asyncQueue1.push({
+//         id: his_trans_data1[0],
+//         addr: his_trans_data1[1],
+//         catfathername: his_trans_data1[2],
+//         catname: his_trans_data1[3],
+//         block: his_trans_data1[4],
+//         rootid: his_trans_data1[5],
+//         price_value: his_trans_data1[6],
+//         date: his_trans_data1[7],
+//         sq_price: his_trans_data1[8],
+//         winloss: his_trans_data1[10],
+//         img_url: his_trans_data1[11],
+//     })
+// });
 
-//data stream new territories
+// //data stream new territories
 
-let instream2 = fs.createReadStream('./data/28housefinal3.csv');
-let outstream2 = new stream;
-outstream2.readable = true;
-outstream2.writeable = true;
+// let instream2 = fs.createReadStream('./data/28housefinal3.csv');
+// let outstream2 = new stream;
+// outstream2.readable = true;
+// outstream2.writeable = true;
 
-var rl2 = readline.createInterface({
-    input: instream2,
-    output: outstream2,
-    terminal: false
-});
+// var rl2 = readline.createInterface({
+//     input: instream2,
+//     output: outstream2,
+//     terminal: false
+// });
 
-const asyncQueue2 = asyncModule.queue(async function(his_trans_data2, cb2) {
-    await
-    console.log(his_trans_data2);
-    let query2 = await knex
-    .select('re_id')
-    .from('real_estate')
-    .where('real_estate.addr', his_trans_data2.addr)
-    .andWhere('catfathername', his_trans_data2.catfathername)
-    .andWhere('catname', his_trans_data2.catname)
+// const asyncQueue2 = asyncModule.queue(async function(his_trans_data2, cb2) {
+//     await
+//     console.log(his_trans_data2);
+//     let query2 = await knex
+//     .select('re_id')
+//     .from('real_estate')
+//     .where('real_estate.addr', his_trans_data2.addr)
+//     .andWhere('catfathername', his_trans_data2.catfathername)
+//     .andWhere('catname', his_trans_data2.catname)
     
-        console.log('do i work?');
-        console.log(query2[0].re_id)
+//         console.log('do i work?');
+//         console.log(query2[0].re_id)
 
-        return await knex
-            .insert({
-                re_id: query2[0].re_id,
+//         return await knex
+//             .insert({
+//                 re_id: query2[0].re_id,
 
-                id: his_trans_data2.id,
+//                 id: his_trans_data2.id,
 
-                block: his_trans_data2.block,
+//                 block: his_trans_data2.block,
 
-                rootid: his_trans_data2.rootid,
+//                 rootid: his_trans_data2.rootid,
 
-                price_value: his_trans_data2.price_value,
+//                 price_value: his_trans_data2.price_value,
 
-                date: his_trans_data2.date,
+//                 date: his_trans_data2.date,
 
-                sq_price: his_trans_data2.sq_price,
+//                 sq_price: his_trans_data2.sq_price,
 
-                winloss: his_trans_data2.winloss,
+//                 winloss: his_trans_data2.winloss,
 
-                img_url: his_trans_data2.img_url
+//                 img_url: his_trans_data2.img_url
 
-            }).into('historical_transaction')
+//             }).into('historical_transaction')
                     
-}, 15);
+// }, 15);
 
-rl2.on('line', function (line2) {
-    let his_trans_data2 = line2.split(",")
-    asyncQueue2.push({
-        id: his_trans_data2[0],
-        addr: his_trans_data2[1],
-        catfathername: his_trans_data2[2],
-        catname: his_trans_data2[3],
-        block: his_trans_data2[4],
-        rootid: his_trans_data2[5],
-        price_value: his_trans_data2[6],
-        date: his_trans_data2[7],
-        sq_price: his_trans_data2[8],
-        winloss: his_trans_data2[10],
-        img_url: his_trans_data2[11],
-    })
-});
+// rl2.on('line', function (line2) {
+//     let his_trans_data2 = line2.split(",")
+//     asyncQueue2.push({
+//         id: his_trans_data2[0],
+//         addr: his_trans_data2[1],
+//         catfathername: his_trans_data2[2],
+//         catname: his_trans_data2[3],
+//         block: his_trans_data2[4],
+//         rootid: his_trans_data2[5],
+//         price_value: his_trans_data2[6],
+//         date: his_trans_data2[7],
+//         sq_price: his_trans_data2[8],
+//         winloss: his_trans_data2[10],
+//         img_url: his_trans_data2[11],
+//     })
+// });
 
-//data stream lantau
+// //data stream lantau
 
-let instream3 = fs.createReadStream('./data/28housefinal170.csv');
-let outstream3 = new stream;
-outstream3.readable = true;
-outstream3.writeable = true;
+// let instream3 = fs.createReadStream('./data/28housefinal170.csv');
+// let outstream3 = new stream;
+// outstream3.readable = true;
+// outstream3.writeable = true;
 
-var rl3 = readline.createInterface({
-    input: instream3,
-    output: outstream3,
-    terminal: false
-});
+// var rl3 = readline.createInterface({
+//     input: instream3,
+//     output: outstream3,
+//     terminal: false
+// });
 
-const asyncQueue3 = asyncModule.queue(async function(his_trans_data3, cb3) {
-    await
-    console.log(his_trans_data3);
-    let query3 = await knex
-    .select('re_id')
-    .from('real_estate')
-    .where('real_estate.addr', his_trans_data3.addr)
-    .andWhere('catfathername', his_trans_data3.catfathername)
-    .andWhere('catname', his_trans_data3.catname)
+// const asyncQueue3 = asyncModule.queue(async function(his_trans_data3, cb3) {
+//     await
+//     console.log(his_trans_data3);
+//     let query3 = await knex
+//     .select('re_id')
+//     .from('real_estate')
+//     .where('real_estate.addr', his_trans_data3.addr)
+//     .andWhere('catfathername', his_trans_data3.catfathername)
+//     .andWhere('catname', his_trans_data3.catname)
     
-        console.log('do i work?');
-        console.log(query3[0].re_id)
+//         console.log('do i work?');
+//         console.log(query3[0].re_id)
 
-        return await knex
-            .insert({
-                re_id: query3[0].re_id,
+//         return await knex
+//             .insert({
+//                 re_id: query3[0].re_id,
 
-                id: his_trans_data3.id,
+//                 id: his_trans_data3.id,
 
-                block: his_trans_data3.block,
+//                 block: his_trans_data3.block,
 
-                rootid: his_trans_data3.rootid,
+//                 rootid: his_trans_data3.rootid,
 
-                price_value: his_trans_data3.price_value,
+//                 price_value: his_trans_data3.price_value,
 
-                date: his_trans_data3.date,
+//                 date: his_trans_data3.date,
 
-                sq_price: his_trans_data3.sq_price,
+//                 sq_price: his_trans_data3.sq_price,
 
-                winloss: his_trans_data3.winloss,
+//                 winloss: his_trans_data3.winloss,
 
-                img_url: his_trans_data3.img_url
+//                 img_url: his_trans_data3.img_url
 
-            }).into('historical_transaction')
+//             }).into('historical_transaction')
                     
-}, 15);
+// }, 15);
 
-rl3.on('line', function (line3) {
-    let his_trans_data3 = line3.split(",")
-    asyncQueue3.push({
-        id: his_trans_data3[0],
-        addr: his_trans_data3[1],
-        catfathername: his_trans_data3[2],
-        catname: his_trans_data3[3],
-        block: his_trans_data3[4],
-        rootid: his_trans_data3[5],
-        price_value: his_trans_data3[6],
-        date: his_trans_data3[7],
-        sq_price: his_trans_data3[8],
-        winloss: his_trans_data3[10],
-        img_url: his_trans_data3[11],
-    })
-});
+// rl3.on('line', function (line3) {
+//     let his_trans_data3 = line3.split(",")
+//     asyncQueue3.push({
+//         id: his_trans_data3[0],
+//         addr: his_trans_data3[1],
+//         catfathername: his_trans_data3[2],
+//         catname: his_trans_data3[3],
+//         block: his_trans_data3[4],
+//         rootid: his_trans_data3[5],
+//         price_value: his_trans_data3[6],
+//         date: his_trans_data3[7],
+//         sq_price: his_trans_data3[8],
+//         winloss: his_trans_data3[10],
+//         img_url: his_trans_data3[11],
+//     })
+// });
