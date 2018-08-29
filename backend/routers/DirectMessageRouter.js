dd26acb6bf291ec6f2c638c689f30e009bd8eb27
siuhangw <wshiahb@gmail.com>
2018-08-29 18:40:16 +0800

    Auth function updates

--- backend/routers/DirectMessageRouter.js
+++ backend/routers/DirectMessageRouter.js
@@ -1,39 +1,39 @@
-const express = require('express');
+const express = require("express");
 
 class DirectMessageRouter {
-    constructor(directMessageService) {
-        this.directMessageService = directMessageService;
-    }
+  constructor(directMessageService) {
+    this.directMessageService = directMessageService;
+  }
 
-    router() {
-        let router = express.Router();
+  router() {
+    let router = express.Router();
 
-        router.get('/messages',this.get.bind(this));
-        router.post('/messages',this.get.bind(this));
-        return router;
-    }
+    router.get("/messages", this.get.bind(this));
+    router.post("/messages", this.get.bind(this));
+    return router;
+  }
 
-    get(req, res) {
-        console.log('Is this getting?')
-        return this.messageService
-            .listConversation(req.query.sender_id,req.query.receiver_id)
-            .then((messages) => res.json(messages))
-            .catch(err => res.status(500).json(err));
-    }
+  get(req, res) {
+    console.log("Is this getting?");
+    return this.messageService
+      .listConversation(req.query.sender_id, req.query.receiver_id)
+      .then(messages => res.json(messages))
+      .catch(err => res.status(500).json(err));
+  }
 
-    post(req, res) {
-        console.log('Is this posting?')
-        return this.messageService
-            .send(rew.body)
-            .then(() => {
-                this.messageService
-                .listConversation(req.body.sender_id, req.body.receiver_id);
-            })
-            .then((messages) => res.json(messages))
-            .catch(err => res.status(500).json(err));
-    }
-        return router;
-    }
+  post(req, res) {
+    console.log("Is this posting?");
+    return this.messageService
+      .send(rew.body)
+      .then(() => {
+        this.messageService.listConversation(
+          req.body.sender_id,
+          req.body.receiver_id
+        );
+      })
+      .then(messages => res.json(messages))
+      .catch(err => res.status(500).json(err));
+  }
 }
 
-module.exports = DirectMessageRouter
+module.exports = DirectMessageRouter;
--- backend/routers/UserRouter.js
+++ backend/routers/UserRouter.js
@@ -1,30 +1,37 @@
-const express = require('express');
+const express = require("express");
 
 class UserRouter {
-    constructor(userService) {
-        this.userService = userService;
-    }
+  constructor(userService) {
+    this.userService = userService;
+  }
 
-    router() {
-        let router = express.Router();
+  router() {
+    let router = express.Router();
 
-        // Profile details service
-        router.get('/details/:userID', (req, res) => { //getting details of user profile
-            this.userService.getUserDetail(req.paramas.userID)
-            .then((userDetails) => res.json(userDetails))
-            .catch((err) => re.status(500).json(err));
-        })
+    // Profile details service
+    router.get("/details/:userID", (req, res) => {
+      //getting details of user profile
+      this.userService
+        .getUserDetail(req.params.userID)
+        .then(userDetails => res.json(userDetails))
+        .catch(err => re.status(500).json(err));
+    });
 
-        router.put('/details/:userID', (req, res) => { //updating the users details
-            this.userService.updateUserDetail(req.paramas.userID,req.body.name,req.body.phone,req.body.email)
-                .then((userDetails) => res.json(userDetails))
-                .catch((err) => res.status(500).json(err));
+    router.put("/details/:userID", (req, res) => {
+      //updating the users details
+      this.userService
+        .updateUserDetail(
+          req.params.userID,
+          req.body.name,
+          req.body.phone,
+          req.body.email
+        )
+        .then(userDetails => res.json(userDetails))
+        .catch(err => res.status(500).json(err));
+    });
 
-        })
-
-
-        return router;
-    }
+    return router;
+  }
 }
 
-module.exports = UserRouter
+module.exports = UserRouter;
