aaf788abcc55cd385d6c069db2f3d29c9ec16c3a
siuhangw <wshiahb@gmail.com>
2018-09-11 14:16:42 +0800

    User favourite add & delete bug fix

--- backend/routers/UserFavRouter.js
+++ backend/routers/UserFavRouter.js
@@ -1,69 +1,101 @@
-const express = require('express');
+const express = require("express");
 
 class UserFavRouter {
-    constructor(userFavService) {
-        this.userFavService = userFavService;
-    }
+  constructor(userFavService) {
+    this.userFavService = userFavService;
+  }
 
-    router () {
-        let router = express.Router();
+  router() {
+    let router = express.Router();
 
-        // Favourite Social Post
-        router.get('/checkpost/:postID', (req, res) => { // check status of social post
-            this.userFavService.isFavSocialPost(req.params.postID)
-                .then((status) => res.json(status))
-                .catch((err) => res.status(500).json(err));
-        })
+    // Favourite Social Post
+    router.get("/checkpost/:postID", (req, res) => {
+      // check status of social post
+      this.userFavService
+        .isFavSocialPost(req.params.postID)
+        .then(status => res.json(status))
+        .catch(err => res.status(500).json(err));
+    });
 
-        router.post('/addpost/:postID', (req, res) => { // add social post to favoutire 
-            this.userFavService.addFavSocialPost(req.params.postID)
-                .then(() => this.userFavService.isFavSocialPost(req.params.postID))
-                    .then((status) => res.json(status))
-                    .catch((err) => res.status(500).json(err));
-        })
+    router.post("/addpost/:postID", (req, res) => {
+      // add social post to favoutire
+      this.userFavService
+        .addFavSocialPost(req.params.postID)
+        .then(() => this.userFavService.isFavSocialPost(req.params.postID))
+        .then(status => res.json(status))
+        .catch(err => res.status(500).json(err));
+    });
 
-        router.delete('/deletepost/:postID' ,(req, res) => { // delete social post from favoutires
-            this.userFavService.deleteFavSocialPost(req.params.postID, )
-                .then(() => this.userFavService.isFavSocialPost(req.params.postID))
-                    .then((status) => res.json(status))
-                    .catch((err) => res.status(500).json(err));
-        })
+    router.delete("/deletepost/:postID", (req, res) => {
+      // delete social post from favoutires
+      this.userFavService
+        .deleteFavSocialPost(req.params.postID)
+        .then(() => this.userFavService.isFavSocialPost(req.params.postID))
+        .then(status => res.json(status))
+        .catch(err => res.status(500).json(err));
+    });
 
-        router.get('/listfavsp/:postID', (req, res) => { // list favoutire social post
-            this.userFavService.listFavSocialPost()
-                .then((social_post) => res.json(social_post))
-                .catch((err) => res.status(500).json(err));
-        })
+    router.get("/listfavsp/:postID", (req, res) => {
+      // list favoutire social post
+      this.userFavService
+        .listFavSocialPost()
+        .then(social_post => res.json(social_post))
+        .catch(err => res.status(500).json(err));
+    });
 
+    // Favourite Flats
+    router.get("/listfavflat/:reID", (req, res) => {
+      // check status of flat
+      this.userFavService
+        .isFavFlat(req.params.reID)
+        .then(status => res.json(status))
+        .catch(err => res.status(500).json(err));
+    });
 
-        // Favourite Flats
-        router.get('/listfavflat/:reID', (req, res) => { // check status of flat
-            this.userFavService.isFavFlat(req.params.reID)
-                .then((status) => res.json(status))
-                .catch((err) => res.status(500).json(err));
-        })
+    router.post("/addflat/:userID/:reID", (req, res) => {
+      // add flat to favoutire
+      console.log("Add Flat");
+      this.userFavService
+        .addFavFlat(req.params.reID, req.params.userID)
+        .then(() =>
+          this.userFavService.isFavFlat(req.params.reID, req.params.userID)
+        )
+        .then(status => res.json(status))
+        .catch(err => res.status(500).json(err));
+    });
 
-        router.post('/addflat/:reID', (req, res) => { // add flat to favoutire 
-            this.userFavService.addFavFlat(req.params.reID, req.params.userID)
-                .then(() => this.userFavService.isFavSocialPost(req.params.reID))
-                    .then((status) => res.json(status))
-                    .catch((err) => res.status(500).json(err));
-        })
+    router.delete("/deleflat/:userID/:reID", (req, res) => {
+      // delete flat from favoutires
+      console.log("Delete Flat");
+      this.userFavService
+        .deleteFavFlat(req.params.reID, req.params.userID)
+        .then(() =>
+          this.userFavService.isFavFlat(req.params.reID, req.params.userID)
+        )
+        .then(status => res.json(status))
+        .catch(err => res.status(500).json(err));
+    });
 
-        router.delete('/deleflat/:reID' ,(req, res) => { // delete flat from favoutires
-            this.userFavService.deleteFavFlat(req.params.reID)
-                .then(() => this.userFavService.isFavSocialPost(req.params.reID))
-                    .then((status) => res.json(status))
-                    .catch((err) => res.status(500).json(err));
-        })
+    // router.get("/watchlist/:userID", (req, res) => {
+    //   // list favoutire flat
+    //   console.log("List Flat");
+    //   this.userFavService
+    //     .listFavFlat(req.params.userID)
+    //     .then(real_estate => res.json(real_estate))
+    //     .catch(err => res.status(500).json(err));
+    // });
 
-        router.get('/watchlist/:reID', (req, res) => { // list favoutire flat
-            this.userFavService.listFavFlat(req.params.userID)
-                .then((real_estate) => res.json(real_estate))
-                .catch((err) => res.status(500).json(err));
-        })
-        return router;
-    }
+    router.get("/watchlist/:userID", (req, res) => {
+      console.log("List Flat");
+      // list favoutire flat
+      this.userFavService
+        .listFlatFavs(req.params.userID)
+        .then(real_estate => res.json(real_estate))
+        .catch(err => res.status(500).json(err));
+    });
+
+    return router;
+  }
 }
 
 module.exports = UserFavRouter;
