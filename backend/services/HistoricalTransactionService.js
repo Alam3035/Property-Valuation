18e34c6a5dcc443ae79213abb508622cb83a6300
sam oshaughnessy <samoshaughnessy123@hotmail.com>
2018-09-18 12:44:17 +0800

    hoope

--- backend/services/HistoricalTransactionService.js
+++ backend/services/HistoricalTransactionService.js
@@ -41,7 +41,7 @@ class HistoricalTransactionService {
                             .where('real_estate.re_id', row.re_id)
                             .whereNot('historical_transaction.winloss', null | 0 | undefined)
                             .orderBy('historical_transaction.winloss', 'desc')
-                            .limit(100)
+                            .limit(50)
                             //could also list by date
                         console.log('selecting two')
 
